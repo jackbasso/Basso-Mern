f19e7201c300b6bf56c15713502cf48421fd5b9a
jackbasso <jackbasso.r@gmail.com>
2022-06-28 15:12:33 -0500

    .gitignore

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,5 +3,26 @@ import express from 'express';
 
 const router = express.Router();
 
+//CREATE
+router.post('/create', (req, res) => {
+  res.send("Yo guardo la orden");
+});
+//UPDATE
+router.put('/update', (req, res) => {
+  res.send("Yo actualizo la orden");
+});
+//DELETE
+router.delete('/delete/:id', (req, res) => {
+  res.send("Yo elimino la orden");
+});
+//GET
+router.get('/:id', (req, res) => {
+  res.send("Yo traigo la orden");
+});
+//GET ALL
+router.get('/', (req, res) => {
+  res.send("Yo muestro todo");
+});
+
 
 export default router;
