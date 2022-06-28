5f56b4271a7fd24e9748026cef6b867fee953352
jackbasso <jackbasso.r@gmail.com>
2022-06-27 21:53:34 -0500

    creación de rutas

--- api/index.js
+++ api/index.js
@@ -1,6 +1,23 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
+dotenv.config()
+
+// Conexión BD
+const connect = async () => {
+  try {
+    await mongoose(process.env.MONGO);
+    console.log("Connected to mongoDB!")
+  } catch (error) {
+    throw error
+  }
+}
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!!")
+})
 
 app.listen(8800, () => {
   console.log("Connected to backend!")
